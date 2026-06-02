29a104aa271797f96e246ce3f0f11ded2bd039c9
Martin Bahl <bahlmartinexactas@gmail.com>
2026-06-02 08:40:11 -0300

    creacion de screen reset-password y de servicio de reset-password

--- src/Screens/ResetPasswordScreen/ResetPasswordScrenn.jsx
+++ src/Screens/ResetPasswordScreen/ResetPasswordScrenn.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navigate, useSearchParams } from 'react-router'
+import resetPassword from '../../services/authService'
 
 const ResetPasswordScrenn = () => {
     const [searchParams, setSearchParams] = useSearchParams()
@@ -9,13 +10,30 @@ const ResetPasswordScrenn = () => {
     if(!reset_password_token){
         return <Navigate to={'/login'} />
     }
+    function onSubmit(formData){
+        if (formData.password !== formData.password_confirmation){
+            alert('Las contraseñas no coinciden')
+            return <Navigate to={'/reset-password?reset_password_token=' + reset_password_token} />
+        }
+        resetPassword(formData.password, reset_password_token)
+    }
+    const { handleSubmit } = useForm(initial_form_state, onSubmit)
     return (
         <div>
-            <h1>Reset Password Screen</h1>
-            <form>
-                
+            <h1>Restablecer Contraseña</h1>
+
+            <form action="" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="password">Nueva contraseña</label>
+                    <input type="password" name='password' id='password' value={formState.password} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="password_confirmation">Confirmar contraseña</label>
+                    <input type="password" name='password_confirmation' id='password_confirmation' value={formState.password_confirmation} onChange={handleChange} />
+                </div>
+
+                <button type='submit'>Restablecer Contraseña</button>
             </form>
-        
         </div>
     )
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-async function login(email,password){
+export async function login(email,password){
     try{
         const response_http = await fetch('http://localhost:8080/api/auth/login',{
         method: 'POST',
@@ -18,4 +18,21 @@ async function login(email,password){
     
 }
 
-export default login
+export async function resetPassword(password, reset_password_token){
+    try{
+            const response_http = await fetch('https://clase-21-backend.vercel.app/api/auth/reset-password',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `bearer ${reset_password_token}`
+            },
+            body: JSON.stringify({
+                password: password
+            })
+     })
+    const response = await response_http.json()
+    console.log(response)
+    }catch(error){
+        throw new Error('Error al restablecer la contraseña')
+    }
+}    
